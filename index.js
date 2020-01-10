eec463ebf16eacc0f5f26987ef63c3e7743f4e95
Redmond Chan <redmondchan92@gmail.com>
2020-01-10 02:05:48 +0000

    added description of problem

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.log("initial commit")
+//write a function that takes in an array of names and return an array of unique names. If the name already exists in the array, add a number behind it to make it unique. Increase the number by 1 if the name repeats again. Ex: input: ["ada", "john", "ada", "ada"]  output: ["ada", "john", "ada1", "ada2"]
+
